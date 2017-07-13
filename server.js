fee138cc32da2ccff04f23ffee99cefa3ee206f5
thayes3116 <timhayes3116@gmail.com>
2017-07-12 22:15:55 -0400

    sign in and login validation is working.  favorites are appearing on all screens

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
-var parseurl = require('parseurl')
-// var passport = require('passport');
-// var flash = require('connect-flash');
-// var morgan = require('morgan');
-//TODO remove me
-// var cookieParser = require('cookie-parser');
-var session = require('express-session');
 
+var sessionStore = require('./config/connection')['sessionStore'];
+var session = require('./config/connection')['session'];
+// console.log(sessionStore);
 
 var app = express();
 var port = process.env.PORT||3000;
@@ -27,36 +23,30 @@ app.use(methodOverride("_method"));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-// pass passport for configuration
-// require('./config/passport')(passport)
 
-// set up our express application
-// app.use(morgan('dev')); // log every requrest to the console
-// app.use(cookieParser()); // read cookies (needed for auth)
-// app.use(bodyParser()); // get information from html forms
-
-// required for passport
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
 
-app.use(function (req, res, next) {
-  var views = req.session.views
+// app.use(function (req, res, next) {
+  // var views = req.session.views
 
-  if (!views) {
-    views = req.session.views = {}
-  }
+//   if (!views) {
+//     views = req.session.views = {}
+//   }
 
-  // get the url pathname
-  var pathname = parseurl(req).pathname
+//   // get the url pathname
+//   var pathname = parseurl(req).pathname
 
-  // count the views
-  views[pathname] = (views[pathname] || 0) + 1
+//   // count the views
+//   views[pathname] = (views[pathname] || 0) + 1
 
-  next()
-})
+//   next()
+// })
 // app.use(passport.initialize()); 
 // app.use(passport.session()); // login sessions
 // app.use(flash()); // use connect-flash for flash messages stroed in session
